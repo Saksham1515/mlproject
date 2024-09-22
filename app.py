1b088d634192424a53a9fc0d54fc19e37155583b
Saksham1515 <sakshamcse2021@gmail.com>
2024-09-22 17:44:14 +0530

    dagshub init

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from src.ml_Project.components.model_trainer import ModelTrainer
 from src.ml_Project.components.data_ingestion import DataIngestion
 from src.ml_Project.components.data_transformation import DataTransformation
 import sys
+import mlflow
+import dagshub
+dagshub.init(repo_owner='Saksham1515', repo_name='mlproject', mlflow=True)
 
 if __name__ == "__main__":
     logging.info("The execution is started~")
@@ -18,6 +21,9 @@ if __name__ == "__main__":
         model_trainer =  ModelTrainer()
         r2_score_value = model_trainer.initiate_model_trainer(train_arr,test_arr)
         print(r2_score_value)
+    
     except Exception as e:
         logging.info("Custom Execption")
         raise CustomException(e,sys)
+    
+    mlflow.set_tracking_uri(uri="https://dagshub.com/Saksham1515/mlproject.mlflow")
